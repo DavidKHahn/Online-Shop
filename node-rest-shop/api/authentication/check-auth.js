f11ee07b1d8bc9320cec49dab2d75e4b6c847b88
David Hahn <dhahn123@gmail.com>
2018-06-25 10:10:54 -0700

    Update check-auth.js

--- node-rest-shop/api/authentication/check-auth.js
+++ node-rest-shop/api/authentication/check-auth.js
@@ -2,6 +2,7 @@ const jwt = require('jsonwebtoken');
 
 module.exports = ( req, res, next ) => {
     try {
+    //splits the header authorization from "bearer + token" to simply "token"
     const token = req.headers.authorization.split(" ")[1];
     // console.log(token); <---to check if we successfully parsed the header
     const decoded = jwt.verify(token, process.env.JWT_KEY);
